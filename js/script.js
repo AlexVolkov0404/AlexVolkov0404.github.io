1f1312eb672a15eb4fa5eb806fba899f457b6baa
volkoval2704 <volkoval2704@gmail.com>
2025-11-01 19:03:36 -0700

    new about update36

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-// Updates only the main content and not side and top nav
 document.addEventListener('DOMContentLoaded', function() {
   const links = document.querySelectorAll('.nav-link');
   const mainContent = document.getElementById('main-content');
 
   links.forEach(link => {
     link.addEventListener('click', function(event) {
-      console.log(link)
       if (!link.href.endsWith('cv.pdf')) {
         event.preventDefault();
 
@@ -13,20 +11,14 @@ document.addEventListener('DOMContentLoaded', function() {
           .then(response => response.text())
           .then(html => {
             mainContent.innerHTML = html;
+
+            // ДОДАНО: перерендер latex
+            if (window.MathJax) {
+              MathJax.typesetPromise();
+            }
           })
           .catch(error => console.error('Error fetching page:', error));
       }
     });
   });
 });
-
-// Abstract button handling 
-function showAbstract(abstractId) {
-  var paragraph = document.getElementById(abstractId);
-
-  if (paragraph.style.display === "none") {
-    paragraph.style.display = "block";
-  } else {
-    paragraph.style.display = "none";
-  }
-}
